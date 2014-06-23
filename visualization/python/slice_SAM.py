78dc58b10c6105d23393fbdc133f61197a24dc59
leonorgg <l.garcia-gutierrez@warwick.ac.uk>
2014-06-23 10:36:25 +0100

    Fixed issue with double color bar in SAM plotting, updated code in slice_SAM.py script

--- visualization/python/slice_SAM.py
+++ visualization/python/slice_SAM.py
@@ -1,9 +1,12 @@
-# Before running this script, calculate velprof_SAM_averaged.dat by running this from /statistics:
-# >> ./average SAM velprof <first file index> <last file index> 1.0 <verbose>
+# Before running this script, calculate velprof_SAM_averaged.dat or SAMconverted_velprof_SAM_averaged.dat by running this from /statistics:
+# >> ./average SAM <name of SAM-formatted file> <first file index> <last file index> 1.0 <verbose>
+#
 #
 # Then, run this from the /python directory with:
 # >> python slice_SAM.py <Name of file in DATA directory, with extension> <Name of output plot (without the extension)>
 # If the name for the output plot is omitted, the result is saved as "output.png"
+# a plot containing information on the number of samples used is also plotted and saved in "output_samples.png", or the corresponding given name 
+# with the suffix "_samples"
 #
 # This generates an image, velprofile.png, of the cross-sectional velocity profile, for example.
 
@@ -11,24 +14,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from mpl_toolkits.mplot3d import Axes3D
-#import numpy as np
+import numpy as np
 import sys
 
 #get header
 if sys.argv[1]!='':
-	if sys.argv[1]=='test':
-		input_file='./tests/velprof_SAM_test2.dat';
-	else:		
-		input_file='./../../DATA/'+sys.argv[1];
+	
+	input_file='./../../DATA/'+sys.argv[1];
 	
 	fh=open(input_file,'r')
 	if len(sys.argv)==3:
 		outputname=sys.argv[2]+".png"
+		outputname_samples=sys.argv[2]+"_samples.png"
 	else:
 		outputname="output.png"
-		print "Saving plot in output.png\n"
+		outputname_samples="output_samples.png"
+		print "Saving plot in output.png and output_samples.png\n"
 else:
-	print "Call as: python slice_SAM.py <name of av file to plot> <name of output plot,without the extension>"
+	print "Call as: python slice_SAM.py <name of av file to plot> <name of output plot, without the extension>"
 	sys.exit(1)
 	
 header=fh.readline()
@@ -40,43 +43,38 @@ nx=int(dimensions[0])
 ny=int(dimensions[1])
 nz=int(dimensions[2])
 
-#get data for the slice to be plotted
-#data=np.genfromtxt(input_file,skip_header=1,delimiter='\t',usecols=(0,1))
+#get data for the slice to be plotted, save in pandas dataframe
+data = pd.read_table(input_file,sep='\t',skiprows=1,skipinitialspace=True,header=None)
 
-data = pd.read_table(input_file,sep='\t',skiprows=1,header=None)
-print data
+#separate columns
+averages=data.values[:,1]
+samples=data.values[:,0]
 
-#averages=data[:,1]
-#samples=data[:,0]
-
-#print averages
-#print samples
 
 #in here, find the max and min of samples, and print to screen
-#print "Maximum number of samples used: ", int(max(samples))," <=total number of samples used in the average"
-#samples_aux=sorted(set(samples)) #remove duplicated entries, return a sorted list
-#print "Minimum number of samples used: ", int(samples_aux[1])
+print "Maximum number of samples used: ", int(max(samples))
+samples_aux=sorted(set(samples)) #remove duplicated entries, return a sorted list
+print "Minimum number of samples used: ", int(samples_aux[1])
 
 #prepare data for plotting
-#samples=samples.reshape(ny,nz)
-#averages=averages.reshape(ny,nz)
+samples=samples.reshape(ny,nz)
+averages=averages.reshape(ny,nz)
+
+#plot samples statistics
+im_samples=plt.imshow(samples,interpolation='nearest')
+plt.colorbar(im_samples, orientation='vertical')
+
+# Save sample statistics plot to disk
+plt.savefig("./"+outputname_samples)#how to save eps?
 
 
-#plot
-#im=plt.imshow(averages,interpolation='nearest')#or bilinear, nearest, bicubic
-#plt.colorbar(im, orientation='vertical')
+#plot averages
+plt.figure(2)
+im_averages=plt.imshow(averages,interpolation='bicubic')#or bilinear, nearest, bicubic
+plt.colorbar(im_averages,orientation='vertical')
 
-# Save it to disk
-#plt.savefig("./"+outputname)#how to save eps?
-
-
-#Show the number of samples used
-#im_samples=plt.imshow(samples,interpolation='nearest')#or bilinear, nearest, bicubic
-#plt.colorbar(im_samples, orientation='vertical')
-#change above: two vertical bars appear. what is the handle for the image?
-
-# Save it to disk
-#plt.savefig("./samples_"+outputname)#how to save eps?
+#save averages plot to disk
+plt.savefig("./"+outputname)#how to save eps?
 
 
 
