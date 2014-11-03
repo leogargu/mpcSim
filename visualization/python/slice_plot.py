774422d0299859e870e3a082be15b66762c89105
Leonor (Godzilla) <l.garcia-gutierrez@warwick.ac.uk>
2014-11-03 12:58:01 +0000

    Committing and pushing because I can't test changes on Godzilla: matplotlib is outdated. Reported to admin and switching to work on Penguintosh

--- visualization/python/slice_plot.py
+++ visualization/python/slice_plot.py
@@ -11,13 +11,23 @@
 # This script generates an image, velprofile.png, of the cross-sectional velocity profile, for example.
 
 import pandas as pd
-import matplotlib.pyplot as plt
-import pylab as pl
 import numpy as np
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 import sys
+import matplotlib.pyplot as plt
+import matplotlib
+print matplotlib.__version__
+
+from matplotlib.colors import from_levels_and_colors
+from matplotlib import ticker
+#import pylab as pl
 from matplotlib.backends.backend_pdf import PdfPages
 #On PdfPages:
 #http://stream.princeton.edu/AWCM/LIBRARIES/matplotlib-1.3.0/lib/matplotlib/backends/backend_pdf.py
+from slice_compare import plot_refs
+
+
+
 
 #define input and output directory
 input_dir = './../../experiments/'
@@ -44,19 +54,41 @@ else:
 	print "Call as: \n>>python slice_plot.py <name of av file to plot, in /experiments> <(optional) name of output plot, without the extension>"
 	sys.exit(1)
 	
+	
+	
 header=fh.readline()
 fh.close()
 
+is_snapshot = False
+is_average = False
+
 # process dimensions of simulation box
 dimensions=header.split()
+if len(dimensions) == 6:
+	print "Plotting a snapshot..."
+	is_snapshot = True
+elif len(dimensions) == 8:
+	print "Plotting an average..."
+	is_average = True
+else:
+	print "WARNING: Check datafile is not corrupt (not plotting snapshot nor average)"
+	sys.exit(1)
+	
+
+
 nx=int(dimensions[0])
 ny=int(dimensions[1])
 nz=int(dimensions[2])
 idx_first_cell = int(dimensions[3])	#global index of the first cell included in the datafile
 idx_last_cell = int(dimensions[4])	#global index of the last cell included in the datafile
-first_file = int(dimensions[5])		#first datafile number used in the average that generated the datafile plotted here 
-stride = int(dimensions[6])		#stride used when calculating the average datafile plotted here
-last_file = int(dimensions[7])		#last datafile number used in the average that generated the datafile plotted here
+#if the datafile is an average, header is: nx ny nz first_cell last_cell first_file stride last_file
+if is_average:
+	first_file = int(dimensions[5])		#first datafile number used in the average that generated the datafile plotted here 
+	stride = int(dimensions[6])		#stride used when calculating the average datafile plotted here
+	last_file = int(dimensions[7])		#last datafile number used in the average that generated the datafile plotted here
+#if the datafile is a snapshot, header is: nx ny nz first_cell last_cell timestep
+if is_snapshot:
+	timestep = int(dimensions[5])
 
 #get data for the slice to be plotted, save in pandas dataframe
 data = pd.read_table(input_file,sep='\t',skiprows=1,skipinitialspace=True,header=None)
@@ -69,9 +101,10 @@ data_min = np.nanmin(averages)
 data_max = np.nanmax(averages)
 
 #in here, find the max and min of samples, and print to screen
-print "Maximum number of samples(SAM)/particles(CAM) used: ", int(max(samples))
-samples_aux=sorted(set(samples)) #remove duplicated entries, return a sorted list
-print "Minimum number of samples(SAM)/particles(CAM) used: ", int(samples_aux[1])
+if is_average:
+	print "Maximum number of samples(SAM)/particles(CAM) used: ", int(max(samples))
+	samples_aux=sorted(set(samples)) #remove duplicated entries, return a sorted list
+	print "Minimum number of samples(SAM)/particles(CAM) used: ", int(samples_aux[1])
 
 #prepare data for plotting
 samples=samples.reshape(ny,nz)
@@ -91,23 +124,25 @@ im_samples=plt.imshow(samples,cmap=my_cmap,interpolation='nearest',extent=[0,nz,
 plt.colorbar(im_samples, orientation='vertical')
 
 #Beautify the axes and ticks, get them to conform to standard reference
-ax=plt.gca()
-ax.arrow(0, ny, nz, 0, fc='k', ec='k',lw = 2,
-         head_width=0.1, head_length=0.2,
-         length_includes_head= True, clip_on = False)
-ax.arrow(0, nz, 0., -nz, fc='k', ec='k',lw = 2, 
-         head_width=0.1, head_length=0.2,
-         length_includes_head= True, clip_on = False)
-ax.get_xaxis().set_ticks([])
-ax.get_yaxis().set_ticks([])
-ax.set_ylabel('Y',fontweight="bold")
-ax.xaxis.set_label_position('top') 
-ax.set_xlabel('Z',fontweight="bold")
+#ax=plt.gca()
+#ax.arrow(0, ny, nz, 0, fc='k', ec='k',lw = 2,
+#         head_width=0.1, head_length=0.2,
+#         length_includes_head= True, clip_on = False)
+#ax.arrow(0, nz, 0., -nz, fc='k', ec='k',lw = 2, 
+#         head_width=0.1, head_length=0.2,
+#         length_includes_head= True, clip_on = False)
+#ax.get_xaxis().set_ticks([])
+#ax.get_yaxis().set_ticks([])
+#ax.set_ylabel('Y',fontweight="bold")
+#ax.xaxis.set_label_position('top') 
+#ax.set_xlabel('Z',fontweight="bold")
 
 # Plot boundary circunference
-boundary = plt.Circle((ny*0.5,nz*0.5),radius=(ny-1)*0.5,linewidth=1,color='k',fill=False)
-fig = plt.gcf()
-fig.gca().add_artist(boundary)
+#boundary = plt.Circle((ny*0.5,nz*0.5),radius=(ny-1)*0.5,linewidth=1,color='k',fill=False)
+#fig = plt.gcf()
+#fig.gca().add_artist(boundary)
+
+plor_refs(plt,ny,nz)
 
 # Save sample statistics plot to disk
 pdffig_samples = PdfPages( output_dir + outputname_samples )
@@ -118,7 +153,10 @@ metadata = pdffig_samples.infodict()
 metadata['Title'] = 'Data plotted =' + sys.argv[1] #input_file 
 metadata['Author'] = 'Script used to plot this = slice_plot.py'
 metadata['Subject']= 'Info on averages plotted in '+outputname
-metadata['Keywords']= 'If data is an average, first_file='+ str(first_file) + ', last_file=' + str(last_file) + ', stride=' + str(stride)
+if is_average:
+	metadata['Keywords']= 'If data is an average, first_file='+ str(first_file) + ', last_file=' + str(last_file) + ', stride=' + str(stride)
+if is_snapshot:
+	metadata['Keywords']='Data is a snapshot taken at timestep='+str(timestep)
 #metadata['Creator'] = 
 #metadata['Producer']=
 pdffig_samples.close()
