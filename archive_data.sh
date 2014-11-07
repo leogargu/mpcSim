d578c012b41631d56fd9423072ffc7734cc131f4
Leonor's Penguintosh <l.garcia-gutierrez@warwick.ac.uk>
2014-11-07 13:01:07 +0000

    Modified slice_plot to plot snapshots (in SAM format only, by definition of snapshot).Created script (cp_experiments.sh) to move data from godzila .../experiments to Penguintosh .../experiments/. Modified archive_data.sh, tested it.

--- archive_data.sh
+++ archive_data.sh
@@ -1,4 +1,4 @@
-#echo $1
+#Checking calling parameter(s) are valid
 if [ "$1" == "" ]; then 
 	echo "Error. Call as: bash archive_data.sh \"data title\" "
 	echo "Aborting... "
@@ -10,19 +10,58 @@ GIT_ID=$(git log --format="%H" -n 1)
 #echo ${GIT_ID}
 
 #Make data subfolder with current date and time
-directory_name=$(date +"%Y.%m.%d_%H.%M")
-mkdir data/$directory_name
+#[Careful with spaces before/after the = sign!!!!]
+target_folder=$(date +"%Y.%m.%d_%H.%M")
+target_directory=./data/$target_folder
+mkdir $target_directory
 
-#Move the data, pbs files and images from /experiments/ to the new folder
-mv ./experiments/* ./data/$directory_name/
-#ERROR: argument list is too long
+#Determine origin of files to be archived
+origin_directory=./experiments/
+
+#Move everything to the new folder
+# First copy the files...
+rsync -aP $origin_directory $target_directory
+# (Run command above again if the transfer stalls r is interrupted: rsync will resume from teh point where it stoppped)
+
+#sanity check
+origin_num=$(find $origin_directory -name "*" | wc -l)
+target_num=$(find $target_directory -name "*" | wc -l)
+
+if [ $origin_num == $target_num ]; then
+	echo "Successfully copied files from "$origin_directory" into "$target_directory
+else
+	echo "Error while copying files. Exiting..."
+	exit 1
+fi
+#...Then delete files inorigin directory, if required. Note rm * can spit a "argument list too long" error, so...
+read -p "DELETE files in "$origin_directory" (yes/no) ? " CONT
+if [ "$CONT" == "yes" ]; then
+	find $origin_directory -maxdepth 1 -name "*.*" -print0 | xargs -0 rm
+else
+	echo "Files in "$origin_directory" have not been removed"
+fi
+
+#http://www.pronego.com/helpdesk/knowledgebase.php?article=59
 
 #write entry in /data/README.txt file with the name of the subfolder and a short title 
 #(comments can be added later by hand) 
 file="./data/README.txt"
 
 #write git commit id into the file. [Note that line 5 must exist in the initial file for this to work!]
-sed -i.bak "5,1i$directory_name - $1 \ngit commit id: $GIT_ID \ncomments: \n" $file
+sed -i.bak "5,1i$target_folder - $1 \ngit commit id: $GIT_ID \ncomments: \n" $file
 
 #print confirmation
-echo "Completed. Add comments on this archived data by editing $file"
+echo "COMPLETED. Add comments on this archived data by editing $file"
+
+###############################################################################
+#NOTES
+i#f [ rm $origin_directory* ]; then
+#		echo "Files removed from "$origin_directory
+#	else
+#		echo "Too many files in "$origin_directory", removing one by one:"
+		
+		#for i in $origin_directory*; do
+	#		echo "Deleting : $i";
+	#		rm $i;
+	#	done
+#	fi
--- data/README.txt
+++ data/README.txt
@@ -2,18 +2,6 @@
 METADATA FOR SUBFOLDERS
 **************************************************************
 
-2014.11.03_20.10 - Data produced with Bedkihal's input 
-git commit id: 69f2097b057ec301acacd1e21fe45c495329aa33 
-comments: 
-
-2014.11.03_16.50 - Bedkihal input data 
-git commit id: 69f2097b057ec301acacd1e21fe45c495329aa33 
-comments: 
-
-
-
-
-
-
-
-
+2014.11.03_20.10 - Data produced with Bedkihal's input
+git commit id: 69f2097b057ec301acacd1e21fe45c495329aa33
+comments:
