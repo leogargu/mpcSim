Fixed rad_avg, structured it in three separate files: radial helpers, rad_avg (giving velocity radial averages with least square fit and density radial averages) and an extra section in slice_plot that plots the cross section with the particle densities

statistics/rad_avg.py
@@ -1,159 +1,57 @@
+import radial_helpers
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-import math
+from matplotlib.backends.backend_pdf import PdfPages
+#import math
 import sys
-
-##############################################
-#Auxiliary functions
-##############################################
-#Finds the local indices of the cells symmetric to the one given by the local index call_idx
-def sym_cells(ny,nz,cell_idx):
-	if nz%2!=0 or ny%2!=0 :
-		print "nz, ny must be even. Aborting...\n";
-		sys.exit(1);
-	nz_half=nz/2;
-	alpha=2*(cell_idx%nz_half)+1;
-	mu=ny*nz_half;
 	
-	return [cell_idx-alpha,cell_idx,2*mu-cell_idx-1,alpha+2*mu-cell_idx-1]
-    
-    
-# Converts a local position vector into the local index of the cell that position belongs to
-# Note: the convention for positions exactly at teh boundary might be different from the C code.
-def local_cellpos2idx(a,ny,nz,point):
-	ainv=1.0/a;
-	coords=[int(point[0]*ainv),int(point[1]*ainv)];
 	
-	return coords[0]*nz+coords[1]
+#http://en.wikipedia.org/wiki/Coefficient_of_determination
+#http://terpconnect.umd.edu/~toh/spectrum/CurveFitting.html
+def calculate_Rsq(yvals, residuals):
+	y_mean=yvals.mean()
+	SStot=0
+	#SSreg=0
+	SSres=0
+	for i in range(0,len(yvals)):
+		SStot += (yvals[i]-y_mean)**2
+		#SSreg += (fit_points[i]-y_mean)**2
+		SSres += (residuals[i])**2
+		Rsq=1-(SSres/SStot)
+	return Rsq
+
+
+#xvals and yvals must be numpy 1D arrays
+#This uses least squares:
+#http://docs.scipy.org/doc/numpy/reference/generated/numpy.polyfit.html
+def calculate_parabolic_fit(xvals,yvals):
+	fit_coeffs, cov = np.polyfit(xvals, yvals, 2, full=False, cov=True)
+	#create fitting polynomial
+	fit = np.poly1d(fit_coeffs)
+	fit_points=[]
+	for x in xvals:
+		fit_points.append(fit(x))
+	residuals = yvals - fit_points
+	R_sq = calculate_Rsq(yvals,residuals)
+	return fit, R_sq
     
     
-# Calculates the arc length between two points p1 and p2 on the same circunference of radius r
-# and center c (the centre is needed only to check that the input points are indeed on the circunference)
-def arc_length(c,r,p1,p2):
-	distSQ1=(c[0]-p1[0])*(c[0]-p1[0])+(c[1]-p1[1])*(c[1]-p1[1]);
-	distSQ2=(c[0]-p2[0])*(c[0]-p2[0])+(c[1]-p2[1])*(c[1]-p2[1]);
-	if (distSQ1-distSQ2) >1e-4:
-		print "arc_length: p1 and p2 are not on the same circunference.Aborting...\n"
-		sys.exit(1);
-	norm=math.sqrt((p1[0]-p2[0])*(p1[0]-p2[0])+(p1[1]-p2[1])*(p1[1]-p2[1]));
-	rinv=1.0/r;
+#auxiliary function to format matedata 
+def sign_to_str(p):
+	if p < 0 :
+		return '-'
+	else:
+		return '+'
 	
-	return 2*r*math.asin(0.5*norm*rinv)
-
-
-
-#There is A LOT of repetition in this code: clean it up.
-def find_weights(a,L,ny,nz,radius):
-	if nz%2!=0 or ny%2!=0 or L%2!=0 :
-		print "nz, ny must be even. Aborting...\n";
-		sys.exit(1);
-	weights=np.empty(ny*nz); 
-	weights.fill(0);    
-	L_half=L/2;    
-	nz_half=nz/2;
-	p1=[L_half-radius,L_half]; #starting point, rightmost cell in quadrant 1
-	plast=[L_half,L_half+radius];
-	cell_last=local_cellpos2idx(a,ny,nz,[plast[0]-0.1*a,plast[1]-0.1*a]);
-	cell_idx= local_cellpos2idx(a,ny,nz,p1);
-	center=[L_half,L_half];
-    
-  	h=L_half+a;
-  	v=int(p1[0]/a)+a;
-    
-   	#points=[p1];
-    
-   	while cell_idx != cell_last:
-   		#print cell_idx
-   	 	crossing_left=0;
-   	 	#exiting current cell from above?
-   	 	yaux=radius*radius-(h-L_half)*(h-L_half);
-   	 	if yaux>=0:
-   	 		y= L_half - math.sqrt( yaux ) ;
-   	 		if abs(y-v)==0:
-   	 			#print "diagonal crossing!\n"
-   	 	 	 	p2=[v,h];
-   	 	 	 	weight=arc_length(center,radius,p1,p2)/(2*radius*math.pi);
-   	 	 	 	cells=sym_cells(ny,nz,cell_idx);
-   	 	 	 	for cell in cells:
-   	 	 	 		weights[cell]=weight;
-   	 	 	 	p1=p2;
-   	 	 	 	cell_idx=cell_idx+nz+1;
-   	 	 	 	h=h+a;
-   	 	 	 	v=v+a;
-   	 	 	 	#points.append(p1);
-   	 	 	 	continue;
-   	 	 	if y<v and y>=(v-a):#...then I am exiting the current cell from above
-   	 	 		p2=[y,h];
-   	 	 	 	#calculate the arc length between p1 and p2
-   	 	 	 	weight=arc_length(center,radius,p1,p2)/(2*radius*math.pi);
-   	 	 	 	cells=sym_cells(ny,nz,cell_idx);
-   	 	 	 	for cell in cells:
-   	 	 	 		weights[cell]=weight;
-   	 	 	 	cell_idx=cell_idx+1;
-   	 	 	 	p1=p2; #python dangerous?
-   	 	 	 	h=h+a;
-   	 	 	else:
-   	 	 		crossing_left=1;
-   	 	else:
-   	 		crossing_left=1;
-        
-        
-		if crossing_left: #I am exiting the current cell from the left
-			zaux= radius*radius - (v-L_half)*(v-L_half)
-			if zaux>=0:
-				z= L_half + math.sqrt( zaux );
-				if abs(z-h)==0:
-					#print "diagonal crossing!\n"
-					p2=[v,h];
-					weight=arc_length(center,radius,p1,p2)/(2*radius*math.pi);
-					cells=sym_cells(ny,nz,cell_idx);
-					for cell in cells:
-						weights[cell]=weight;
-					p1=p2;
-					cell_idx=cell_idx+nz+1;
-					h=h+a;
-					v=v+a;
-					#points.append(p1);
-					continue;
-                    
-        	        	if z<=h and z>(h-a):
-        	        		p2=[v,z];
-        	        		#calculate arc length between p1 and p2
-        	        		weight=arc_length(center,radius,p1,p2)/(2*radius*math.pi);
-        	        		cells=sym_cells(ny,nz,cell_idx);
-        	        		for cell in cells:
-        	        			weights[cell]=weight;
-        	        		cell_idx=cell_idx+nz;
-        	        		p1=p2;
-        	        		v=v+a;
-        	        	else:
-        	        		print "Error. Attempted to cross left but couldn't. Aborting...\n"
-        	        		sys.exit(1);
-        	        else:
-        	        	print "Error. Not knowing where to cross. Aborting...\n"
-        	        	sys.exit(1);
-        	#points.append(p1);
-        #Out of while loop: add the arc in the last cell:
-        weight=arc_length(center,radius,p1,plast)/(2*radius*math.pi);
-        cells=sym_cells(ny,nz,cell_idx);
-        for cell in cells:
-        	weights[cell]=weight;
-        #points.append(plast);
-        
-    	#print cell_idx
-    
-    	return weights #,points
-    
 ##############################################
 # Main program
 ##############################################
 
 # check number of calling arguments
-if len(sys.argv)<6:
-	print "call as: \n>>python rad_avg.py <full path and name of data file in /experiments> <Value of a in the simulation> <Value of L in the simulation> <radius over which to perform the average><viscosity /mu/ >\n"
+if len(sys.argv)<5:
+	print "call as: \n>>python rad_avg.py <name of data file in /experiments/> <Value of a in the simulation> <Value of L in the simulation> <Number of circunferences over which to perform the radial average><(Optional) Name of the output figure>\n"
 	sys.exit(1)
 
 
@@ -165,8 +63,18 @@ output_dir = input_dir
 input_file = sys.argv[1];
 a = float(sys.argv[2]);
 L = float(sys.argv[3]);
-radius = float(sys.argv[4]);
-viscosity=float(sys.arg[5]);
+R = (L-a)*0.5;
+L_half = 0.5*L;
+num_points = int(sys.argv[4])/2;
+if len(sys.argv)==6:
+	outputname=sys.argv[5]+".pdf" #change the extension if required
+	outputname_density=sys.argv[5]+"_density.pdf"
+else:
+	outputname="output.pdf"
+	outputname_density = 'output_density.pdf'
+	print "Plots will be saved as output.pdf and output_density.pdf\n"
+
+#Note that the number of points is not strictly followed (+-1)
 
 #get header
 fh = open( input_dir + input_file,'r')
@@ -182,54 +90,140 @@ nz=int(dimensions[2])
 
 #get slice data
 data = pd.read_table(input_dir+input_file,sep='\t',skiprows=1,skipinitialspace=True,header=None);
+#for diagnostic purposes mainly, we'll also do the radial average of the particle density
+num_particles = data.values[:,0];
 
-#Get weights
-warray=find_weights(a,L,ny,nz,radius);
-
-#Check weights have been calculated correctly
-assert (warray.sum()-1.0) <1e-6
-
-#Calculate average over the circunference, and print to screen
-#print np.dot(np.nan_to_num(data.values[:,1]),warray)
-average = np.average(np.nan_to_num(data.values[:,1]),weights=warray)
-print "Weighted mean:", average;
-
-#Calculate the weighted standard deviation
-#Not clear what expression to follow, undecided about how to correct bias for small samples. For example:
-#http://www.itl.nist.gov/div898/software/dataplot/refman2/ch2/weightsd.pdf
-# Here will follow GNU GSL:
-#http://www.gnu.org/software/gsl/manual/html_node/Weighted-Samples.html
-V2=np.dot(warray,warray);
-V1=warray.sum();
-average_array=np.empty(ny*nz); 
-average_array.fill(average); 
-diffSQ=np.square( np.subtract(np.nan_to_num(data.values[:,1]) , average_array) );
-print "Weighted std: ", ( V1 / (V1*V1-V2) )* np.dot(warray,diffSQ)
+#Get radial average points
+y_values = []
+x_vals = []
+for i in range(0,num_points):
+	radius = a + i*(R-a)/(1.0*num_points-1.0);
+	x_vals.insert(0,L_half-radius)
+	x_vals.append(L_half+radius)
+	lengths = radial_helpers.find_seg_quant(a,L,ny,nz,radius,'length')
+	warray=radial_helpers.find_weights(lengths,radius)
+	y_values.append(np.dot(np.nan_to_num(data.values[:,1]),warray))
+y_vals=np.concatenate((y_values[::-1],y_values))
 
 
+#Calculate the best parabolic fit (least squares)
+fit,R2 = calculate_parabolic_fit(x_vals,y_vals)
+fit_points=[]
+fit_x_values=np.linspace(0 ,ny , 100)
+for x in fit_x_values:
+    fit_points.append(fit(x))    
+    
+   
+print "R^2 = ", R2
+print "Slip = ", np.array(y_vals).min()
+
+    
+#Plotting
+plt.figure(1)
+plt.plot(x_vals,y_vals, 'ro', markersize=3)
+fig=plt.gcf()
+ax=fig.gca()
+ax.plot(fit_x_values,fit_points)
+
+y1=y_vals.max()
+ax.set_ylim([0,1.1*y1])
+plt.vlines(range(0,ny,1),0,1.1*y1)
+plt.vlines([0.5,ny-0.5],0,1.1*y1,'g')
+ax.set_xlim(ax.get_xlim()[::-1]) 
 
 
-################################################################
-#Merge me properly later - This is work in progress
+ax.set_xlabel('Y (cells)',fontweight="bold")
+ax.set_ylabel('v_x',fontweight="bold") #Latex?		
 
-#generate average points:
-app_points = np.empty(10); 
-app_points.fill(0);
 
-R = (L-a)*0.5
+#save averages plot to disk, prepare metadata object first
+pdffig = PdfPages( output_dir + outputname )
+plt.savefig( pdffig, format="pdf")#output_dir + outputname )
 
-for i in range(0,11):
-	r = a + i*(R-3*a)*0.05
-	warray=find_weights(a,L,ny,nz,r)
-	assert (warray.sum()-1.0) <1e-6
-	average = np.average(np.nan_to_num(data.values[:,1]),weights=warray)
-	app_points[i]=average
+#add metadata to figure
+metadata = pdffig.infodict()
+metadata['Title'] = 'Data plotted =' + input_dir + input_file #input_file 
+metadata['Author'] = 'Script used to plot this = rad_avg.py.py'
 
-Vmax=np.nanmax(data.values[:,1])
-# plot several radial averages and compare with pot of videal=Vmax(1-(r/R)^2)
 
-x=np.linspace(-R-1,R+1)
-y=Vmax*(1-(x/R)*(x/R))
-plt.plot(x, )
+metadata['Subject']= ' Radial averages and least squares parabolic fit = '+ str(fit[0]) + sign_to_str(fit[1]) +str(fit[1])+'x'+ sign_to_str(fit[2]) + str(fit[2])+'x^2'
+metadata['Keywords']= 'R^2 = '+ str(R2)
+#metadata['Creator'] = 
+#metadata['Producer']=
+
+
+#Save figure
+pdffig.close()
+
+
+##########################################
+#Now the particle densities
+plt.figure(2)
+
+volumes = radial_helpers.find_seg_quant(a,L,ny,nz,R,'volume')
+particle_densities = np.divide(num_particles, volumes)
+
+
+
+#Get radial average points
+y_values = []
+x_vals = []
+num_points *= 3
+for i in range(1,num_points+1):
+	radius = i*R/(1.0*num_points);
+	x_vals.insert(0,L_half-radius)
+	x_vals.append(L_half+radius)
+	lengths = radial_helpers.find_seg_quant(a,L,ny,nz,radius,'length')
+	warray = radial_helpers.find_weights(lengths,radius)
+	y_values.append(np.dot(particle_densities,warray))
+y_vals=np.concatenate((y_values[::-1],y_values))
+
+
+plt.plot(x_vals,y_vals, 'ro', markersize=3)
+fig=plt.gcf()
+ax=fig.gca()
+
+yaux=y_vals.mean()
+y1=yaux*1.8
+
+#y1=y_vals.max()
+ax.set_ylim([0,1.1*y1])
+plt.vlines(range(0,ny,1),0,1.1*y1)
+plt.vlines([0.5,ny-0.5],0,1.1*y1,'g')
+ax.set_xlim(ax.get_xlim()[::-1]) 
+
+
+ax.set_xlabel('Y (cells)',fontweight="bold")
+ax.set_ylabel('particle density',fontweight="bold") #Latex?		
+
+
+
+#save averages plot to disk, prepare metadata object first
+pdffig = PdfPages( output_dir + outputname_density )
+plt.savefig( pdffig, format="pdf")
+
+#add metadata to figure
+metadata = pdffig.infodict()
+metadata['Title'] = 'Data plotted =' + input_dir + input_file #input_file 
+metadata['Author'] = 'Script used to plot this = rad_avg.py.py and radial_helpers.py'
+
+
+metadata['Subject']= ' Radial averages of the particle density '
+#metadata['Keywords']= ''
+#metadata['Creator'] = 
+#metadata['Producer']=
+
+
+#Save figure
+pdffig.close()
+
+
+
+
+
+
+
+
+
 
 

visualization/python/slice_plot.py
@@ -3,7 +3,7 @@
 #
 #
 # Then, run this from the /python directory with:
-# >> python slice_plot.py <Name of file in /experiments directory, with extension> <(optional) Name of output plot (without the extension)>
+# >> python slice_plot.py <Name of file in /experiments directory, with extension> <value of a><(optional) Name of output plot (without the extension)>
 # If the name for the output plot is omitted, the result is saved as "output.png"
 # a plot containing information on the number of samples used (for SAM averages) or number of particles across samples (for CAM averages)
 # is also plotted and saved in "output_samples.png" , or the corresponding given name with the suffix "_samples.png".
@@ -24,6 +24,12 @@ from matplotlib.backends.backend_pdf import PdfPages
 #On PdfPages:
 #http://stream.princeton.edu/AWCM/LIBRARIES/matplotlib-1.3.0/lib/matplotlib/backends/backend_pdf.py
 #from slice_compare import plot_refs (something didn't work well: error message is taken from slice_compare instead of slice_plot)
+
+
+#Get the radial helpers
+sys.path.insert(0, './../../statistics/')
+import radial_helpers
+
 
 # Auxiliary functions
 def plot_refs(plt,ny,nz):
@@ -56,24 +62,28 @@ empty_color_stats='black'
 empty_color_avgs='white'
 
 #check health of input arguments and get header
-if len(sys.argv)>=2 and sys.argv[1]!='':
+if len(sys.argv)>=3 and sys.argv[1]!='':
 	
 	input_file=input_dir + sys.argv[1];
 	
 	fh=open(input_file,'r')
-	if len(sys.argv)==3:
-		outputname=sys.argv[2]+".pdf" #change the extension if required
-		outputname_samples=sys.argv[2]+"_samples.pdf"
+	if len(sys.argv)==4:
+		outputname=sys.argv[3]+".pdf" #change the extension if required
+		outputname_samples=sys.argv[3]+"_samples.pdf"
+		outputname_densities=sys.argv[3]+"_density.pdf"
 	else:
 		outputname="output.pdf"
 		outputname_samples="output_samples.pdf"
-		print "Plots will be saved in output.pdf and output_samples.pdf\n"
+		outputname_densities="output_density.pdf"
+		print "Plots will be saved in output.pdf, output_samples.pdf, output_density.pdf\n"
 else:
 	print "Call as: \n>>python slice_plot.py <name of av file to plot, in /experiments> <(optional) name of output plot, without the extension>"
 	sys.exit(1)
 	
 	
-	
+a=float(sys.argv[2]);
+
+
 header=fh.readline()
 fh.close()
 
@@ -113,25 +123,26 @@ data = pd.read_table(input_file,sep='\t',skiprows=1,skipinitialspace=True,header
 
 #separate columns
 averages=data.values[:,1]
-samples=data.values[:,0]
+num_particles=data.values[:,0]
 
 data_min = np.nanmin(averages)
 data_max = np.nanmax(averages)
 
 #in here, find the max and min of samples, and print to screen
 if is_average:
-	print "Maximum number of samples(SAM)/particles(CAM) used: ", int(max(samples))
-	samples_aux=sorted(set(samples)) #remove duplicated entries, return a sorted list
+	print "Maximum number of samples(SAM)/particles(CAM) used: ", int(max(num_particles))
+	samples_aux=sorted(set(num_particles)) #remove duplicated entries, return a sorted list
 	print "Minimum number of samples(SAM)/particles(CAM) used: ", int(samples_aux[1])
 
 #prepare data for plotting
-samples=samples.reshape(ny,nz)
+samples=num_particles.reshape(ny,nz)
 averages=averages.reshape(ny,nz)
 
 ##########################################
 #The following two sections could be rewritten as 1-2 functions?
 ##########################################
 #plot samples statistics
+plt.figure(1)
 samples[samples==0] = np.nan
 
 my_cmap=plt.get_cmap('Greens') #Also interesting: jet, spectral, rainbow 
@@ -150,7 +161,7 @@ plt.savefig( pdffig_samples, format="pdf" )
 
 #add metadata to figure
 metadata = pdffig_samples.infodict()
-metadata['Title'] = 'Data plotted =' + sys.argv[1] #input_file 
+metadata['Title'] = 'Data plotted =' + input_file
 metadata['Author'] = 'Script used to plot this = slice_plot.py'
 metadata['Subject']= 'Info on averages plotted in '+outputname
 if is_average:
@@ -161,10 +172,42 @@ if is_snapshot:
 #metadata['Producer']=
 pdffig_samples.close()
 
+##########################################
+#plot densities
+plt.figure(2)
+L = ny*a; #or nz
+R = 0.5*(L-a)
+volumes = radial_helpers.find_seg_quant(a,L,ny,nz,R,'volume')
+particle_densities = np.divide(num_particles, volumes)
+p_density = particle_densities.reshape(ny,nz)
+
+my_cmap = plt.get_cmap('nipy_spectral') #Also interesting: gnuplot2, spectral, rainbow variants 
+density_plot = plt.imshow(p_density,cmap=my_cmap,interpolation='nearest',extent=[0,nz,0,ny])
+plt.colorbar(density_plot, orientation='vertical')
+
+plot_refs(plt,ny,nz)
+
+# Save sample statistics plot to disk
+pdffig_density = PdfPages( output_dir + outputname_densities )
+plt.savefig( pdffig_density, format="pdf" )
+
+#add metadata to figure
+metadata = pdffig_density.infodict()
+metadata['Title'] = 'Particle densities. Data plotted =' + input_file
+metadata['Author'] = 'Script used to plot this = slice_plot.py and radia_helpers.py'
+metadata['Subject']= 'Info on averages plotted in '+outputname
+if is_average:
+	metadata['Keywords']= 'If data is an average, first_file='+ str(first_file) + ', last_file=' + str(last_file) + ', stride=' + str(stride)
+if is_snapshot:
+	metadata['Keywords']='Data is a snapshot taken at timestep='+str(timestep)
+metadata['Creator'] = 'Particle densities calculated for each collicion cell as: num particles/volume inside lumen'
+#metadata['Producer']=
+pdffig_density.close()
+
 
 ##########################################
 #plot averages or snapshot
-plt.figure(2)
+plt.figure(3)
 my_cmap=plt.get_cmap('jet') #Also interesting: jet, spectral, rainbow 
 masked_array = np.ma.array(averages, mask=np.isnan(averages))
 my_cmap.set_bad(color=empty_color_avgs)
@@ -182,7 +225,7 @@ plt.savefig( pdffig, format="pdf")#output_dir + outputname )
 
 #add metadata to figure
 metadata = pdffig.infodict()
-metadata['Title'] = 'Data plotted =' + sys.argv[1] #input_file 
+metadata['Title'] = 'Data plotted =' + input_file 
 metadata['Author'] = 'Script used to plot this = slice_plot.py'
 metadata['Subject']= 'Info on samples(SAM)/particles(CAM) plotted in '+outputname_samples
 if is_average: