Fixes for averaging and plotting routines

statistics/rad_avg.py
@@ -50,25 +50,25 @@ def sign_to_str(p):
 ##############################################
 
 # check number of calling arguments
-if len(sys.argv)<5:
-	print "call as: \n>>python rad_avg.py <name of data file in /experiments/> <Value of a in the simulation> <Value of L in the simulation> <Number of circunferences over which to perform the radial average><(Optional) Name of the output figure>\n"
+if len(sys.argv)<6:
+	print "call as: \n>>python rad_avg.py <dir, relative to calling directory> <name of data file> <Value of a in the simulation> <Value of L in the simulation> <Number of circunferences over which to perform the radial average><(Optional) Name of the output figure>\n"
 	sys.exit(1)
 
 
 #define input and output directory
-input_dir = './../experiments/'
+input_dir = sys.argv[1]# './../experiments/'
 output_dir = input_dir
 
 #Get arguments
-input_file = sys.argv[1];
-a = float(sys.argv[2]);
-L = float(sys.argv[3]);
+input_file = sys.argv[2];
+a = float(sys.argv[3]);
+L = float(sys.argv[4]);
 R = (L-a)*0.5;
 L_half = 0.5*L;
-num_points = int(sys.argv[4])/2;
-if len(sys.argv)==6:
-	outputname=sys.argv[5]+".pdf" #change the extension if required
-	outputname_density=sys.argv[5]+"_density.pdf"
+num_points = int(sys.argv[5])/2;
+if len(sys.argv)==7:
+	outputname=sys.argv[6]+".pdf" #change the extension if required
+	outputname_density=sys.argv[6]+"_density.pdf"
 else:
 	outputname="output.pdf"
 	outputname_density = 'output_density.pdf'
@@ -82,11 +82,17 @@ header=fh.readline()
 fh.close()
 
 # process dimensions of simulation box
-dimensions=header.split('\t')[:3]
+dimensions=header.split('\t')[:8]
 nx=int(dimensions[0])
 ny=int(dimensions[1])
 nz=int(dimensions[2])
 
+first_file = int(dimensions[5])		#first datafile number used in the average that generated the datafile plotted here 
+stride = int(dimensions[6])		#stride used when calculating the average datafile plotted here
+last_file = int(dimensions[7])	
+	
+num_samples = ((last_file-first_file)/stride)+1;
+assert isinstance(num_samples,int), "rad_avg.py.py: error calculating number of samples"
 
 #get slice data
 data = pd.read_table(input_dir+input_file,sep='\t',skiprows=1,skipinitialspace=True,header=None);
@@ -115,8 +121,12 @@ for x in fit_x_values:
     
    
 print "R^2 = ", R2
-print "Slip = ", np.array(y_vals).min()
+print "Slip (from data) = ", np.array(y_vals).min()
+print "Slip (from fit) =", fit(0.5*a)
+print "Fit: p[1]="  + str(fit[0]) + ", p[x]= "+ str(fit[1])+', p[x^2]= '+ str(fit[2])
 
+#For latex:
+print num_samples , " & " , '%.6f'%fit[0] , " & ", '%.6f'%fit[1], ' & ', '%.6f'%fit[2], " & " , '%.6f'%R2 , " \\\\"
     
 #Plotting
 plt.figure(1)
@@ -146,8 +156,8 @@ metadata['Title'] = 'Data plotted =' + input_dir + input_file #input_file
 metadata['Author'] = 'Script used to plot this = rad_avg.py.py'
 
 
-metadata['Subject']= ' Radial averages and least squares parabolic fit = '+ str(fit[0]) + sign_to_str(fit[1]) +str(fit[1])+'x'+ sign_to_str(fit[2]) + str(fit[2])+'x^2'
-metadata['Keywords']= 'R^2 = '+ str(R2)
+metadata['Subject']= ' Radial averages and least squares parabolic fit = '+ str(fit[0]) + sign_to_str(fit[1]) +str(abs(fit[1]))+'x'+ sign_to_str(fit[2]) + str(abs(fit[2]))+'x^2'
+metadata['Keywords']= 'R^2 = '+ str(R2)+' , Slip (fit)= '+str(fit(0.5*a))+', Slip (data)= '+str(np.array(y_vals).min())
 #metadata['Creator'] = 
 #metadata['Producer']=
 
@@ -160,11 +170,11 @@ pdffig.close()
 #Now the particle densities
 plt.figure(2)
 
-volumes = radial_helpers.find_seg_quant(a,L,ny,nz,R,'volume')
-particle_densities = np.divide(num_particles, volumes)
-
-
-
+volumes = radial_helpers.find_seg_quant(a,L,ny,nz,R,'volume')	
+num_particles_cell = np.divide(num_particles,num_samples)
+particle_densities = np.divide(num_particles_cell, volumes)
+	
+	
 #Get radial average points
 y_values = []
 x_vals = []

visualization/python/equilibration.py
@@ -1,5 +1,5 @@
 # Run this from the /python directory with:
-# >> python equilibration.py 
+# >> python equilibration.py <(optional) directory of equilibration.py>
 #
 # This generates an image, momentum.pdf, of the evolution of total momentum at each timestep in the simulation
 
@@ -14,7 +14,11 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 
 #define input and output directory
-input_dir = './../../experiments/'
+if len(sys.argv) > 1:
+	input_dir = sys.argv[1];
+else:
+	input_dir = './../../experiments/'
+print "Taking data and saving plot in " + input_dir	
 output_dir = input_dir
 inputname='equilibration.dat'
 outputname='momentum.pdf'
@@ -25,9 +29,9 @@ data=np.genfromtxt(input_dir + inputname ,delimiter='\t')
 #plot momentum
 plt.plot(data[:,:-1]) #last column is the energy
 plt.legend(["Jx","Jy","Jz"],loc=7)
-plt.xlabel("timestep")
+plt.xlabel("time steps")
 plt.ylabel("Total linear momentum J")
-plt.title("Equilibration phase")
+#plt.title("Equilibration phase")
 
 
 # Save it to disk, and add metadata

visualization/python/slice_compare.py
@@ -1,7 +1,8 @@
 # This script calculates (SAM average - CAM average) and plots it in the same way that either average is plotted
 #
 # Run this from the python directory with:
-# >> python slice_compare.py <Name of SAM averaged file in /experiments directory, with extension> <Name of CAM averaged file in /experiments directory, with extension> <(optional) name of output image, without extension>
+# >> python slice_compare.py <Name of SAM averaged file in /experiments directory, with extension> <Name of CAM averaged file in /experiments directory, 
+#	with extension> <(optional) name of output image, without extension>
 # If the name for the output plot is omitted, the result is saved as "output_compare.pdf"
 #
 
@@ -16,6 +17,11 @@ from matplotlib.backends.backend_pdf import PdfPages
 #On PdfPages:
 #http://stream.princeton.edu/AWCM/LIBRARIES/matplotlib-1.3.0/lib/matplotlib/backends/backend_pdf.py        
         
+        
+#Get the radial helpers
+sys.path.insert(0, './../../statistics/')
+import radial_helpers
+
 # Auxiliary functions
 def plot_refs(plt,ny,nz):
 	#Beautify the axes and ticks, get them to conform to standard reference
@@ -39,12 +45,12 @@ interpolation_setting='nearest' #also possible: bilinear, bicubic
                
 # check number of calling arguments
 if len(sys.argv)<3:
-	print "call as: \n>>python slice_compare.py <name of SAM file in /experiments> <name of CAM file in /experiments> <(optional) name output file, without extension>\n"
+	print "call as: \n>>python slice_compare.py <dir, relative to calling directory> <name of SAM file in /experiments> <name of CAM file in /experiments> <(optional) name output file, without extension>\n"
 	sys.exit(1)
 	
 	
 #define input and output directory
-input_dir = './../../experiments/'
+input_dir = sys.argv[1] #'./../../experiments/'
 output_dir = input_dir
 
 # Define color of empty cells
@@ -52,20 +58,20 @@ empty_color_diff='gray'
 empty_color_avgs='white'
 
 #get header
-if sys.argv[1]!='':
+if sys.argv[2]!='':
 	
-	input_file_sam = input_dir + sys.argv[1];
-	input_file_cam = input_dir + sys.argv[2];
+	input_file_sam = input_dir + sys.argv[2];
+	input_file_cam = input_dir + sys.argv[3];
 	
 	fh_sam=open(input_file_sam,'r')
 	#fh_cam=open(input_file_cam,'r') # currently the script does not check that the sam and cam data have been obtained from the same raw datafiles.
 	
-	if len(sys.argv)==4:
-		outputname=sys.argv[3]+".pdf" #change format if needed
-		print "Plots will be saved as " + outputname + ", cam_" + outputname + ", and sam_" + outputname
+	if len(sys.argv)==5:
+		outputname=sys.argv[4]+".pdf" #change format if needed
+		print "Plots will be saved as " + outputname + ", cam_" + outputname + ", sam_" + outputname + ', and diffradavg_' + outputname
 	else:
 		outputname="output_compare.pdf"
-		print "Plots will be saved as output_compare.pdf, cam_output_compare.pdf and sam_output_compare.pdf"
+		print "Plots will be saved as output_compare.pdf, cam_output_compare.pdf, sam_output_compare.pdf and diffradavg_output_compare.pdf"
 	print "In ", output_dir
 else:
 	print "Call as: python slice_compare.py <name of SAM av file> <name of CAM av file> <name of output plot, without the extension>"
@@ -105,10 +111,10 @@ if np.nanmax(averages_cam) > my_vmax:
 	my_vmax = np.nanmax(averages_cam)	
 
 #calculate difference
-difference=np.subtract(averages_sam,averages_cam)
+difference_1d=np.subtract(averages_sam,averages_cam)
 
 #prepare data for plotting
-difference = difference.reshape(ny,nz)
+difference = difference_1d.reshape(ny,nz)
 averages_cam = averages_cam.reshape(ny,nz)
 averages_sam = averages_sam.reshape(ny,nz)
 
@@ -141,7 +147,7 @@ plt.savefig( pdffig, format="pdf")
 
 #add metadata to figure
 metadata = pdffig.infodict()
-metadata['Title'] = 'Data plotted (2nd column) = difference ' + sys.argv[1] +' [MINUS] '+ sys.argv[2] #input_file_sam, input_file_cam
+metadata['Title'] = 'Data plotted (2nd column) = difference ' + sys.argv[1]+sys.argv[2] +' [MINUS] '+sys.argv[1] +sys.argv[3] #input_file_sam, input_file_cam
 metadata['Author'] = 'Script used to plot this = slice_compare.py'
 metadata['Subject']= 'Info on CAM and SAM avgs plotted in cam_'+outputname+' and sam_'+outputname
 metadata['Keywords']= 'If data is an average, first_file='+ str(first_file) + ', last_file=' + str(last_file) + ', stride=' + str(stride)
@@ -149,9 +155,66 @@ metadata['Keywords']= 'If data is an average, first_file='+ str(first_file) + ',
 #metadata['Producer']=
 pdffig.close()
 
+
+##########################################
+#Radial average of comparison plot
+
+#Get radial average points
+y_values = []
+x_vals = []
+num_points = 7*ny;
+a = 1.0;  #<--------------------------------------------------------------------THIS SHOULD BE GIVEN AS A PARAMETER
+L = ny*a;
+L_half = 0.5*L;
+R = 0.5*(L-a);
+
+for i in range(0,num_points):
+	radius = a + i*(R-a)/(1.0*num_points-1.0);
+	x_vals.insert(0,L_half-radius)
+	x_vals.append(L_half+radius)
+	lengths = radial_helpers.find_seg_quant(a,L,ny,nz,radius,'length')
+	warray=radial_helpers.find_weights(lengths,radius)
+	y_values.append(   np.dot( np.nan_to_num(difference_1d)  ,  warray )   )
+y_vals=np.concatenate((y_values[::-1],y_values))
+
+
+#Plot
+plt.figure(2)
+plt.plot(x_vals,y_vals, 'ro', markersize=3)
+fig=plt.gcf()
+ax=fig.gca()
+
+y_m = y_vals.min()
+y_M = y_vals.max()
+
+ax.set_ylim([0.9*y_m,1.1*y_M])
+plt.vlines(range(0,ny,1),0.9*y_m,1.1*y_M)
+plt.vlines([0.5,ny-0.5],0.9*y_m,1.1*y_M,'g')
+ax.set_xlim(ax.get_xlim()[::-1]) 
+
+
+ax.set_xlabel('Y (cells)',fontweight="bold")
+ax.set_ylabel('v_x(SAM)-v_x(CAM)',fontweight="bold") #Latex?		
+
+#save averages plot to disk, prepare metadata object first
+pdffig = PdfPages( output_dir + 'diffradavg_' + outputname )
+plt.savefig( pdffig, format="pdf")
+
+#add metadata to figure
+metadata = pdffig.infodict()
+metadata['Title'] = 'Data plotted is radial average of SAM - CAM velocity averages'
+metadata['Author'] = 'Script used to plot this = slice_compare.py.py and radial_helpers.py'
+metadata['Subject']= ' Datafiles: '+input_file_sam + ' , '+input_file_cam
+#metadata['Keywords']= ''
+#metadata['Creator'] = 
+#metadata['Producer']=
+
+#Save figure
+pdffig.close()
+
 #========================================
 #auxiliary plot 1/2 (SAM data only)
-plt.figure(2)
+plt.figure(3)
 
 # plot empty cells in a pre-determined color
 my_cmap=plt.get_cmap('jet') #Also interesting: jet, spectral, rainbow 
@@ -169,7 +232,7 @@ plt.savefig( pdffig_sam, format="pdf" )
 
 #add metadata to figure
 metadata = pdffig_sam.infodict()
-metadata['Title'] = 'Data plotted (2nd column) =' + sys.argv[1] #input_file_sam
+metadata['Title'] = 'Data plotted (2nd column) =' + sys.argv[1]+sys.argv[2] #input_file_sam
 metadata['Author'] = 'Script used to plot this = slice_compare.py'
 metadata['Subject']= 'Info on corresponding CAM avg plotted in cam_'+outputname+', difference in '+ outputname
 metadata['Keywords']= 'If data is an average, first_file='+ str(first_file) + ', last_file=' + str(last_file) + ', stride=' + str(stride)
@@ -180,7 +243,7 @@ pdffig_sam.close()
 
 #========================================
 #auxiliary plot 2/2 (CAM data only)
-plt.figure(3)
+plt.figure(4)
 
 # plot empty cells in a pre-determined color
 my_cmap=plt.get_cmap('jet') #Also interesting: jet, spectral, rainbow 
@@ -199,7 +262,7 @@ plt.savefig( pdffig_cam , format="pdf")
 
 #add metadata to figure
 metadata = pdffig_cam.infodict()
-metadata['Title'] = 'Data plotted (2nd column) =' + sys.argv[2] #input_file_cam
+metadata['Title'] = 'Data plotted (2nd column) =' + sys.argv[1]+sys.argv[3] #input_file_cam
 metadata['Author'] = 'Script used to plot this = slice_compare.py'
 metadata['Subject']= 'Info on corresponding SAM avg plotted in sam_'+outputname+', difference in '+ outputname
 metadata['Keywords']= 'If data is an average, first_file='+ str(first_file) + ', last_file=' + str(last_file) + ', stride=' + str(stride)

visualization/python/slice_plot.py
@@ -54,7 +54,7 @@ interpolation_setting='nearest' #also possible: bilinear, bicubic
  
 
 #define input and output directory
-input_dir = './../../experiments/'
+input_dir = sys.argv[1] # './../../experiments/'
 output_dir = input_dir
 
 # Define color of empty cells
@@ -62,26 +62,26 @@ empty_color_stats='black'
 empty_color_avgs='white'
 
 #check health of input arguments and get header
-if len(sys.argv)>=3 and sys.argv[1]!='':
+if len(sys.argv)>=4 and sys.argv[2]!='':
 	
-	input_file=input_dir + sys.argv[1];
+	input_file=input_dir + sys.argv[2];
 	
 	fh=open(input_file,'r')
-	if len(sys.argv)==4:
-		outputname=sys.argv[3]+".pdf" #change the extension if required
-		outputname_samples=sys.argv[3]+"_samples.pdf"
-		outputname_densities=sys.argv[3]+"_density.pdf"
+	if len(sys.argv)==5:
+		outputname=sys.argv[4]+".pdf" #change the extension if required
+		outputname_samples=sys.argv[4]+"_samples.pdf"
+		outputname_densities=sys.argv[4]+"_density.pdf"
 	else:
 		outputname="output.pdf"
 		outputname_samples="output_samples.pdf"
 		outputname_densities="output_density.pdf"
 		print "Plots will be saved in output.pdf, output_samples.pdf, output_density.pdf\n"
 else:
-	print "Call as: \n>>python slice_plot.py <name of av file to plot, in /experiments> <(optional) name of output plot, without the extension>"
+	print "Call as: \n>>python slice_plot.py <dir> <name of av file to plot, in /experiments> <value of a> <(optional) name of output plot, without the extension>"
 	sys.exit(1)
 	
 	
-a=float(sys.argv[2]);
+a=float(sys.argv[3]);
 
 
 header=fh.readline()
@@ -165,7 +165,7 @@ metadata['Title'] = 'Data plotted =' + input_file
 metadata['Author'] = 'Script used to plot this = slice_plot.py'
 metadata['Subject']= 'Info on averages plotted in '+outputname
 if is_average:
-	metadata['Keywords']= 'If data is an average, first_file='+ str(first_file) + ', last_file=' + str(last_file) + ', stride=' + str(stride)
+	metadata['Keywords']= 'Data is an average, first_file='+ str(first_file) + ', last_file=' + str(last_file) + ', stride=' + str(stride)
 if is_snapshot:
 	metadata['Keywords']='Data is a snapshot taken at timestep='+str(timestep)
 #metadata['Creator'] = 
@@ -174,46 +174,98 @@ pdffig_samples.close()
 
 ##########################################
 #plot densities
-plt.figure(2)
-L = ny*a; #or nz
-R = 0.5*(L-a)
-volumes = radial_helpers.find_seg_quant(a,L,ny,nz,R,'volume')
-particle_densities = np.divide(num_particles, volumes)
-p_density = particle_densities.reshape(ny,nz)
+if 'CAM' in sys.argv[2]:
+	#Sanity check
+	assert is_average, 'slice_plot.py: wrong average file name'
+	
+	plt.figure(2)
+	L = ny*a; #or nz
+	R = 0.5*(L-a)
+	volumes = radial_helpers.find_seg_quant(a,L,ny,nz,R,'volume')
+	num_samples = ((last_file-first_file)/stride)+1;
+	assert isinstance(num_samples,int), "slice_plot.py: error calculating number of samples"
+	
+	num_particles_cell = np.divide(num_particles,num_samples)
+	particle_densities = np.divide(num_particles_cell, volumes)
+	
+	p_density = particle_densities.reshape(ny,nz)
 
-my_cmap = plt.get_cmap('nipy_spectral') #Also interesting: gnuplot2, spectral, rainbow variants 
-density_plot = plt.imshow(p_density,cmap=my_cmap,interpolation='nearest',extent=[0,nz,0,ny])
-plt.colorbar(density_plot, orientation='vertical')
+	my_cmap = plt.get_cmap('nipy_spectral') #Also interesting: gnuplot2, spectral, rainbow variants 
+	density_plot = plt.imshow(p_density,cmap=my_cmap,interpolation='nearest',extent=[0,nz,0,ny])
+	plt.colorbar(density_plot, orientation='vertical')
 
-plot_refs(plt,ny,nz)
+	plot_refs(plt,ny,nz)
 
-# Save sample statistics plot to disk
-pdffig_density = PdfPages( output_dir + outputname_densities )
-plt.savefig( pdffig_density, format="pdf" )
+	# Save sample statistics plot to disk
+	pdffig_density = PdfPages( output_dir + outputname_densities )
+	plt.savefig( pdffig_density, format="pdf" )
 
-#add metadata to figure
-metadata = pdffig_density.infodict()
-metadata['Title'] = 'Particle densities. Data plotted =' + input_file
-metadata['Author'] = 'Script used to plot this = slice_plot.py and radia_helpers.py'
-metadata['Subject']= 'Info on averages plotted in '+outputname
-if is_average:
+	#add metadata to figure
+	metadata = pdffig_density.infodict()
+	metadata['Title'] = 'Particle densities. Data plotted =' + input_file
+	metadata['Author'] = 'Script used to plot this = slice_plot.py and radia_helpers.py'
+	metadata['Subject']= 'Info on averages plotted in '+outputname
 	metadata['Keywords']= 'If data is an average, first_file='+ str(first_file) + ', last_file=' + str(last_file) + ', stride=' + str(stride)
-if is_snapshot:
-	metadata['Keywords']='Data is a snapshot taken at timestep='+str(timestep)
-metadata['Creator'] = 'Particle densities calculated for each collicion cell as: num particles/volume inside lumen'
-#metadata['Producer']=
-pdffig_density.close()
+	
+	#Temporary?###############################
+	plt.figure(3)
+	#volumes = radial_helpers.find_seg_quant(a,L,ny,nz,R,'volume')
+	#num_samples = ((last_file-first_file)/stride)+1;
+	#assert isinstance(num_samples,int), "slice_plot.py: error calculating number of samples"
+	
+	num_particles_cell = np.divide(num_particles,num_samples)
+	#particle_densities = np.divide(num_particles_cell, volumes)
+	
+	p_density = num_particles_cell.reshape(ny,nz)
+
+	my_cmap = plt.get_cmap('nipy_spectral') #Also interesting: gnuplot2, spectral, rainbow variants 
+	density_plot = plt.imshow(p_density,cmap=my_cmap,interpolation='nearest',extent=[0,nz,0,ny])
+	plt.colorbar(density_plot, orientation='vertical')
+
+	plot_refs(plt,ny,nz)
+
+	# Save sample statistics plot to disk
+	pdffig_density = PdfPages( output_dir + sys.argv[4]+"_particle_density.pdf" )
+	plt.savefig( pdffig_density, format="pdf" )
+
+	#add metadata to figure
+	#metadata = pdffig_density.infodict()
+	#metadata['Title'] = 'Particle densities. Data plotted =' + input_file
+	#metadata['Author'] = 'Script used to plot this = slice_plot.py and radia_helpers.py'
+	#metadata['Subject']= 'Info on averages plotted in '+outputname
+	#metadata['Keywords']= 'If data is an average, first_file='+ str(first_file) + ', last_file=' + str(last_file) + ', stride=' + str(stride)
+	#####################################
+	pdffig_density.close()
 
 
 ##########################################
 #plot averages or snapshot
-plt.figure(3)
-my_cmap=plt.get_cmap('jet') #Also interesting: jet, spectral, rainbow 
-masked_array = np.ma.array(averages, mask=np.isnan(averages))
-my_cmap.set_bad(color=empty_color_avgs)
+num_levels = 40
+vmin, midpoint, vmax = np.nanmin(averages), 0.0,  np.nanmax(averages)
+levels = np.linspace(vmin, vmax, num_levels)
+midp = np.mean(np.c_[levels[:-1], levels[1:]], axis=1)
+vals = np.interp(midp, [vmin, midpoint, vmax], [0, 0.5, 1])
+colors = plt.cm.seismic(vals)
+cmap, norm = from_levels_and_colors(levels, colors)  #won't use the norm
 
-im_averages=plt.imshow(averages,cmap=my_cmap,vmin=data_min,vmax=data_max,interpolation=interpolation_setting, extent=[0,nz,0,ny])#or bilinear, nearest, bicubic
-plt.colorbar(im_averages,orientation='vertical')
+masked_array = np.ma.array(averages, mask=np.isnan(averages))
+cmap.set_bad(color='black')
+plt.figure(4)
+fig, ax = plt.subplots()
+im_averages = ax.imshow(masked_array, vmin=vmin, vmax=vmax,cmap=cmap,interpolation='nearest',extent=[0,nz,0,ny])
+cb=fig.colorbar(im_averages,orientation='vertical')
+cb.locator = ticker.MaxNLocator(nbins=num_levels/3,symmetric=True,prune=None)
+cb.update_ticks()
+
+#################
+
+#plt.figure(3)
+#my_cmap=plt.get_cmap('jet') #Also interesting: jet, spectral, rainbow 
+#masked_array = np.ma.array(averages, mask=np.isnan(averages))
+#my_cmap.set_bad(color=empty_color_avgs)
+
+#im_averages=plt.imshow(averages,cmap=my_cmap,vmin=data_min,vmax=data_max,interpolation=interpolation_setting, extent=[0,nz,0,ny])#or bilinear, nearest, bicubic
+#plt.colorbar(im_averages,orientation='vertical')
 
 #Beautify the axes and ticks, get them to conform to standard reference
 plot_refs(plt,ny,nz)